93269817e9763d3a74be3ab28f2dc80970572aa1
Konrad Erbetowski <Konrad.Erbetowski@gnb.pl>
2024-02-08 20:06:02 +0100

    portfolio changes; CV update

--- src/containers/Portfolio.tsx
+++ src/containers/Portfolio.tsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from "react";
-import LazyLoad from "react-lazyload";
-import PortfolioItem from "../components/PortfolioItem";
-import { TechnologiesEnum } from "../models/TechnologiesEnum";
-import {EnterAnimationService} from '../services/enter-animation.service';
+import React, { useEffect, useState } from 'react';
+import LazyLoad from 'react-lazyload';
+import PortfolioItem from '../components/PortfolioItem';
+import { TechnologiesEnum } from '../models/TechnologiesEnum';
+import { EnterAnimationService } from '../services/enter-animation.service';
 
 interface Project {
   id: string;
@@ -20,7 +20,7 @@ const Portfolio = () => {
 
   const projects: Project[] = [
     {
-      id: "pzu",
+      id: 'pzu',
       technologies: [
         TechnologiesEnum.angular,
         TechnologiesEnum.ts,
@@ -31,30 +31,23 @@ const Portfolio = () => {
         TechnologiesEnum.css,
       ],
       commercial: true,
-      collaborateCompany: "Speednet Sp. z o.o.",
-      collaborateCompanyUrl: "https://speednet.pl/",
-      referenceUrl: "https://www.portalcash.pl/",
-      images: ["pzu_lp_0.png", "pzu_lp_1.png", "pzu_lp_2.png", "pzu_lp_3.png"],
+      collaborateCompany: 'Speednet Sp. z o.o.',
+      collaborateCompanyUrl: 'https://speednet.pl/',
+      referenceUrl: 'https://www.portalcash.pl/',
+      images: ['pzu_lp_0.png', 'pzu_lp_1.png', 'pzu_lp_2.png', 'pzu_lp_3.png'],
     },
     {
-      id: "bancovo",
-      technologies: [
-        TechnologiesEnum.angular,
-        TechnologiesEnum.ts,
-        TechnologiesEnum.sass,
-        TechnologiesEnum.html5,
-        TechnologiesEnum.js,
-        TechnologiesEnum.php,
-        TechnologiesEnum.css,
-      ],
+      id: 'bancovo',
+      technologies: [TechnologiesEnum.angular, TechnologiesEnum.ts, TechnologiesEnum.sass],
       commercial: true,
-      collaborateCompany: "Speednet Sp. z o.o.",
-      collaborateCompanyUrl: "https://speednet.pl/",
-      referenceUrl: "https://speednet.pl/portfolio-post/bancovo-creation-of-an-innovative-fintech-platform/",
-      images: ["bancovo_0.png"],
+      collaborateCompany: 'Speednet Sp. z o.o.',
+      collaborateCompanyUrl: 'https://speednet.pl/',
+      referenceUrl:
+        'https://speednet.pl/portfolio-post/bancovo-creation-of-an-innovative-fintech-platform/',
+      images: ['bancovo_0.png'],
     },
     {
-      id: "saar",
+      id: 'saar',
       technologies: [
         TechnologiesEnum.angular,
         TechnologiesEnum.ts,
@@ -62,33 +55,29 @@ const Portfolio = () => {
         TechnologiesEnum.docker,
       ],
       commercial: true,
-      collaborateCompany: "Speednet Sp. z o.o.",
-      collaborateCompanyUrl: "https://speednet.pl/",
-      referenceUrl: "https://speednet.pl/portfolio-post/saar-digital-transformation/",
+      collaborateCompany: 'Speednet Sp. z o.o.',
+      collaborateCompanyUrl: 'https://speednet.pl/',
+      referenceUrl: 'https://speednet.pl/portfolio-post/saar-digital-transformation/',
       images: [
-        "saar_0.png",
-        "saar_1.png",
-        "saar_2.png",
-        "saar_3.png",
-        "saar_4.png",
-        "saar_5.png",
-        "saar_6.png",
-        "saar_7.png",
+        'saar_0.png',
+        'saar_1.png',
+        'saar_2.png',
+        'saar_3.png',
+        'saar_4.png',
+        'saar_5.png',
+        'saar_6.png',
+        'saar_7.png',
       ],
     },
     {
-      id: "bbs3d",
-      technologies: [
-        TechnologiesEnum.angular,
-        TechnologiesEnum.ts,
-        TechnologiesEnum.sass,
-      ],
+      id: 'bbs3d',
+      technologies: [TechnologiesEnum.angular, TechnologiesEnum.ts, TechnologiesEnum.sass],
       commercial: true,
-      collaborateCompany: "Speednet Sp. z o.o.",
-      collaborateCompanyUrl: "https://speednet.pl/",
-      referenceUrl: "https://speednet.pl/portfolio-post/cbs-operational-e-learning-platform/",
-        // "https://www.buildingblocksofscience.com/ion-bbs3d-at-home-learning-114JL-45147J.html",
-      images: ["bbs3d_0.png", "bbs3d_1.png", "bbs3d_2.png"],
+      collaborateCompany: 'Speednet Sp. z o.o.',
+      collaborateCompanyUrl: 'https://speednet.pl/',
+      referenceUrl: 'https://speednet.pl/portfolio-post/cbs-operational-e-learning-platform/',
+      // "https://www.buildingblocksofscience.com/ion-bbs3d-at-home-learning-114JL-45147J.html",
+      images: ['bbs3d_0.png', 'bbs3d_1.png', 'bbs3d_2.png'],
     },
     // {
     //   id: "crypto-news",
@@ -120,7 +109,7 @@ const Portfolio = () => {
 
   return (
     <div id="portfolio-container" className={`portfolio-container`}>
-      <div className={`portfolio-wrapper ${animateOnEnter ? "open" : ""}`}>
+      <div className={`portfolio-wrapper ${animateOnEnter ? 'open' : ''}`}>
         {projects.map((project: Project, index: number) => {
           return (
             <LazyLoad key={`portfolio-item-${index}`} offset={100}>
